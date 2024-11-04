5ac9d42846f47fb2cc0fa58252b55f0161630428
unknown <panaceaw0w@gmail.com>
2024-11-04 11:25:53 +0400

    Отключение js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Функция для получения списка веток
-async function fetchBranches() {
-    const response = await fetch('https://api.github.com/repos/olaynick/100gramm/branches');
-    const branches = await response.json();
-    const branchesList = document.getElementById('branches-list');
+// // Функция для получения списка веток
+// async function fetchBranches() {
+//     const response = await fetch('https://api.github.com/repos/olaynick/100gramm/branches');
+//     const branches = await response.json();
+//     const branchesList = document.getElementById('branches-list');
 
-    branches.forEach(branch => {
-        const li = document.createElement('li');
-        // Создаем ссылку на соответствующую страницу ветки на GitHub Pages
-        li.innerHTML = `<a href="https://olaynick.github.io/100gramm/${branch.name}/index.html">${branch.name}</a>`;
-        branchesList.appendChild(li);
-    });
-}
+//     branches.forEach(branch => {
+//         const li = document.createElement('li');
+//         // Создаем ссылку на соответствующую страницу ветки на GitHub Pages
+//         li.innerHTML = `<a href="https://olaynick.github.io/100gramm/${branch.name}/index.html">${branch.name}</a>`;
+//         branchesList.appendChild(li);
+//     });
+// }
 
-// Вызов функции для получения веток
-fetchBranches();
+// // Вызов функции для получения веток
+// fetchBranches();
