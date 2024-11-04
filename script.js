1c4f1e13d67e09175b2391813238b512e4784acd
unknown <panaceaw0w@gmail.com>
2024-11-04 11:14:43 +0400

    Изменение пути к репозиториям

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Функция для получения списка веток
 async function fetchBranches() {
-    const response = await fetch('https://api.github.com/repos/ваш-логин/ваш-репозиторий/branches');
+    const response = await fetch('https://api.github.com/repos/olaynick/100gramm/branches');
     const branches = await response.json();
     const branchesList = document.getElementById('branches-list');
 
     branches.forEach(branch => {
         const li = document.createElement('li');
-        li.innerHTML = `<a href="https://github.com/ваш-логин/ваш-репозиторий/tree/${branch.name}">${branch.name}</a>`;
+        li.innerHTML = `<a href="https://github.com/olaynick/100gramm/tree/${branch.name}">${branch.name}</a>`;
         branchesList.appendChild(li);
     });
 }
